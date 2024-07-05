e64c1d3dc6dbd0df32bc0e6e3495c1cb12dd459c
Сергей Журавский <117652478+sergei-zhuravskii@users.noreply.github.com>
2024-07-05 18:05:21 +0300

    Add files via upload

---
+++ js/showing/show-notice.js
@@ -0,0 +1,37 @@
+
+// function show-notice
+
+function showNotice() {
+
+  const noticeLinks = document.querySelectorAll('.notice-link'); // notice-links iniaialisation
+
+  noticeLinks.forEach(link => {   // event for click by links
+    link.addEventListener('click', openModal);
+  })
+
+  // function show notice
+  function openModal() {
+
+    const modalContainer = document.querySelector('.notice__modal-container');
+    const modalBtn = document.querySelector('.modal__btn');
+    const body = document.querySelector('body');
+
+    modalContainer.classList.remove('hidden');
+    modalBtn.addEventListener('click', closeModal);
+    modalContainer.addEventListener('click', closeModal);
+    body.classList.add('body-scroll-off');
+    body.classList.add('body-offset');
+
+    function closeModal() {
+      modalContainer.classList.add('hidden');
+      body.classList.remove('body-scroll-off');
+      body.classList.remove('body-offset');
+    }
+
+  }
+
+}
+
+// export functions
+
+export default showNotice; // show notice
